8b69c9bd6fac8b2c7cbc91b613dd6926afe37085
Georgy <gerizch@rambler.ru>
2022-12-08 17:49:16 +0300

    add Floyd's algoritm

---
+++ src/algoritms/FloydsCycleDetection/DuplicateFinder.cs
@@ -0,0 +1,32 @@
+﻿namespace FloydsCycleDetection
+{
+    public class DuplicateFinder
+    {
+
+        public static int FindDuplicateNumber(int[] numbersArr)
+        {
+            var tortoise = numbersArr[0];
+            var hare = numbersArr[0];
+
+            while (true)
+            {
+                tortoise = numbersArr[tortoise];
+                hare = numbersArr[numbersArr[hare]];
+                
+                if (tortoise == hare) { break; }
+            }
+
+            var pointer1 = numbersArr[0];
+            var pointer2 = tortoise;
+
+            while (pointer1 != pointer2)
+            {
+                pointer1 = numbersArr[pointer1];
+                pointer2 = numbersArr[pointer2];
+            }
+
+            return pointer1;
+
+        }
+    }
+}
---
+++ src/algoritms/FloydsCycleDetection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FloydsCycleDetection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DuplicateFinder.FindDuplicateNumber(new int[] {3, 1, 1, 4, 2});
+            Console.WriteLine(result);
+        }
+    }
+}
